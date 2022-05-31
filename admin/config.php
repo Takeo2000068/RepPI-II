eac3d139cc058dca5aa52371d53c3904735e0ff7
Takeo2000068 <2000068@aluno.univesp.br>
2022-05-31 10:53:04 -0300

    commit inicial

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://univesp-pi.com.br/pi-ii/admin/');
+define('HTTP_CATALOG', 'http://univesp-pi.com.br/pi-ii/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://univesp-pi.com.br/pi-ii/admin/');
+define('HTTPS_CATALOG', 'http://univesp-pi.com.br/pi-ii/');
+
+// DIR
+define('DIR_APPLICATION', '/home/univesp-pi/www/pi-ii/admin/');
+define('DIR_SYSTEM', '/home/univesp-pi/www/pi-ii/system/');
+define('DIR_IMAGE', '/home/univesp-pi/www/pi-ii/image/');
+define('DIR_STORAGE', '/home/univesp-pi/storage/');
+define('DIR_CATALOG', '/home/univesp-pi/www/pi-ii/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.univesp-pi.com.br');
+define('DB_USERNAME', 'univesppi08');
+define('DB_PASSWORD', 'univesppi08');
+define('DB_DATABASE', 'univesppi08');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
